ae0e40a778f247491ccd89579dfa5f3109be9160
Reynadi Andreas Priesley <rey@reynadi.com>
2025-06-06 21:05:12 +0700

    chore: updating csp policy for unpkg

--- next.config.mjs
+++ next.config.mjs
@@ -37,7 +37,7 @@ const nextConfig = {
 
 const ContentSecurityPolicy = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' app.rybbit.io;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' app.rybbit.io unpkg.com;
     style-src 'self' 'unsafe-inline';
     img-src * blob: data:;
     media-src 'none';
